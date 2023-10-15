7dce0faaea3d94bab388d41b4a75435d6af709ed
Hira Khan <phr.hirakhan@gmail.com>
2023-10-16 00:19:07 +0200

    update_recipe is now modularized in separate python file

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,3 +1,5 @@
 from .login import login
 from .list_recipes import list_down_recipe
 from .delete_recipe import delete_recipe
+from .create_recipe import create_recipe
+from .update_recipe import update_recipe
