46e663639f64c056d4b8708268acc0b88aa643a9
Hira Khan <phr.hirakhan@gmail.com>
2023-10-17 22:30:17 +0200

    How to run the program inserted in run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+"""
+RUN
+---
+
+Run is the starting point of the code try following
+command on terminal to run the code
+
+python run.py
+"""
 from src.features import main_menu, login
 from src.globals import StateVariables
 
